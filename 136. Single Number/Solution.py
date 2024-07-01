02511a0aced19c3b41c07c37d379b98f5a8c7d6b
Lakshita Mahajan <66481666+lakshita15@users.noreply.github.com>
2024-06-30 20:47:12 -0400

    Python Solution
            Submission Statistics:
            Question #: 136
            Language: Python
            Runtime: 96 ms
            Runtime percentile: 57.46
            Memory: 13.768 MB
            Memory percentile: 66.44

--- 136. Single Number/Solution.py
+++ 136. Single Number/Solution.py
@@ -5,6 +5,12 @@ class Solution(object):
         :rtype: int
         """
 
-        for i in range(len(nums)):
-             if(nums.count(nums[i])==1):
+        # for i in range(len(nums)):
+        #      if(nums.count(nums[i])==1):
+        #         return nums[i]
+        nums.sort()
+        for i in range(0, len(nums) - 1, 2):
+            if nums[i] != nums[i+1]:
                 return nums[i]
+           
+        return nums[-1]
